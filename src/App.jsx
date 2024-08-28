3219e68958067112ea37803c772c99b17fe37bc9
Al-Amin <alaminkhanshakil@gmail.com>
2024-08-28 12:17:22 +0600

    Add input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
+import Input from "./ui/input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
+      <StyledApp>
         <H1>The Wild Oasis</H1>
         <Button>Check in</Button>
-      </div>
+        <Input placeholder="Number of guests" type="text" />
+      </StyledApp>
     </>
   );
 }
