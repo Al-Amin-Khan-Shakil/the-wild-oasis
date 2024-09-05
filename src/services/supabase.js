89ed55be7190388a5bf4bd011dc6be4ae6f903bb
Al-Amin <alaminkhanshakil@gmail.com>
2024-09-05 11:29:37 +0600

    Connect the project with supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://rqqcfjbqkbxclevmsuyb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxcWNmamJxa2J4Y2xldm1zdXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUxMDc4OTksImV4cCI6MjA0MDY4Mzg5OX0.gozPhQ0Xj481xrTLjUfwpgbF5snkcc1UZ4JmkBUmEAE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
